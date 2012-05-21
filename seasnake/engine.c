3b9794039ed9b0179c934504659df9962069fdbc
Chakrit Wichian <chakrit@oozou.com>
2012-05-21 23:44:33 +0700

    Move engine code to engine.c

--- seasnake/engine.c
+++ seasnake/engine.c
@@ -1,11 +1,50 @@
 #include "engine.h"
+#include <curses.h>
+#include "point.h"
+
+
+void
+initCurses() {
+  initscr();  // standard curses init
+  cbreak();   // turns-off console buffering
+  noecho();   // echo-less input
+}
+
+void
+deInitCurses() {
+  endwin();
+}
+
 
 void
 initEngine() {
-
+  initCurses();
 }
 
 void
 deInitEngine() {
+  deInitCurses();
+}
 
+
+void
+engineStart() {
+  Point *p = newPoint(7, 7);
+  char ch;
+
+  while ((ch = getch()) != 'x') {
+    mvaddch(0, 0, ch);
+    mvaddch(p->y, p->x, ' ');
+
+    switch (ch) {
+      case 'i': p->y--; break;
+      case 'j': p->x--; break;
+      case 'k': p->y++; break;
+      case 'l': p->x++; break;
+    }
+
+    mvaddch(p->y, p->x, 'x');
+  }
+
+  freePoint(p);
 }
--- seasnake/seasnake.c
+++ seasnake/seasnake.c
@@ -6,41 +6,10 @@
 #include "engine.h"
 
 
-void
-initCurses() {
-  initscr();  // standard curses init
-  cbreak();   // turns-off console buffering
-  noecho();   // echo-less input
-}
-
-void
-deInitCurses() {
-  endwin();
-}
-
-
 int
 main() {
-  Point *p = newPoint(7, 7);
-  char ch;
-
-  initCurses();
-
-  while ((ch = getch()) != 'x') {
-    mvaddch(0, 0, ch);
-    mvaddch(p->y, p->x, ' ');
-
-    switch (ch) {
-      case 'i': p->y--; break;
-      case 'j': p->x--; break;
-      case 'k': p->y++; break;
-      case 'l': p->x++; break;
-    }
-
-    mvaddch(p->y, p->x, 'x');
-  }
-
-  freePoint(p);
-  deInitCurses();
+  initEngine();
+  engineStart();
+  deInitEngine();
   return 0;
 }
